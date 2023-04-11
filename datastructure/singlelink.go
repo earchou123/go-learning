d2178a1e1b3e6b07745deba70e0093ad892a598f
Zerlinda <earchou123@gmail.com>
2023-04-11 11:57:58 +0800

    add InsertNodeByNo

--- datastructure/singlelink.go
+++ datastructure/singlelink.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 type Node struct {
+	no   int
 	data string
 	next *Node
 }
@@ -21,7 +22,7 @@ func ListLink(head *Node) {
 func InsertNode(head *Node, node *Node) {
 	tmpNode := head
 	for {
-		if tmpNode.next == nil {
+		if tmpNode.next == nil { //找到队尾，跳出循环
 			break
 		}
 		tmpNode = tmpNode.next
@@ -29,19 +30,56 @@ func InsertNode(head *Node, node *Node) {
 	tmpNode.next = node
 }
 
-func main() {
+func InsertNodeByNo(head *Node, node *Node) {
+	tmpNode := head
+	flag := true
+	for {
+		if tmpNode.next == nil { //找到队尾，跳出循环
+			break
+		} else if node.no == tmpNode.no { // 链中有该no，不插入
+			flag = false
+			break
+		} else if node.no < tmpNode.next.no { // 插入到tmpNode后面
+			break
+		}
+		tmpNode = tmpNode.next
+	}
+	if flag {
+		node.next = tmpNode.next
+		tmpNode.next = node
+	} else {
+		fmt.Printf("node.no[%d] 已存在\n", node.no)
+	}
+}
 
+func main() {
+	node := Node{}
 	node1 := Node{
+		no:   1,
 		data: "node1",
 	}
 	node2 := Node{
+		no:   2,
 		data: "node2",
 	}
 	node3 := Node{
+		no:   3,
 		data: "node3",
 	}
-	InsertNode(&node1, &node2)
-	InsertNode(&node1, &node3)
-	ListLink(&node1)
+	node4 := Node{
+		no:   4,
+		data: "node4",
+	}
+	node5 := Node{
+		no:   3,
+		data: "node5",
+	}
+	InsertNode(&node, &node1)
+	InsertNode(&node, &node2)
+	InsertNode(&node, &node4)
+	ListLink(&node)
 
+	InsertNodeByNo(&node, &node3)
+	InsertNodeByNo(&node, &node5)
+	ListLink(&node)
 }
